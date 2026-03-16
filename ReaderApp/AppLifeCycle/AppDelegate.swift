1aa67d50bd517e482df623c23b0bd2b56169a1a3
Bishwajit Kumar <biswajit125>
2026-03-16 11:26:36 +0530

    add new update

--- ReaderApp/AppLifeCycle/AppDelegate.swift
+++ ReaderApp/AppLifeCycle/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-    // added new changes
+    // added new changes hsdgshdgfjsdgfj
     // MARK: - UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
